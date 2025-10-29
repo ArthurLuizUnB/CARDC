aca6864c58f5e3078fa78a4352f605d1f35ca59e
Arthur Luiz <164208479+ArthurLuizUnB@users.noreply.github.com>
2025-10-29 07:13:18 -0300

    Update init_db_script.py

--- MusicBeta_V02/Main/init_db_script.py
+++ MusicBeta_V02/Main/init_db_script.py
@@ -2,9 +2,5 @@ from app import app, init_db
 import sys
 
 # Executa a função de inicialização do BD
-try:
-    with app.app_context():
-        init_db()
-except Exception as e:
-    print(f"ERRO CRÍTICO NA INICIALIZAÇÃO DO BANCO: {e}")
-    sys.exit(1)
+with app.app_context():
+    init_db()
